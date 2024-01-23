e3b0ea62587dbae1528581927aee0b05857bd4ff
Ananda <anandawirahadikusuma@gmail.com>
2024-01-23 14:53:45 +0700

    scnd commit ( membuat button navigate ke halaman selanjutnya )

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LoginView'),
+        centerTitle: true,
+      ),
+      body:  Center(
+        child: Form(
+          key: controller.formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: controller.usernameController,
+                decoration: InputDecoration(hintText: "Masukkan Username"),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "username tidak boleh kosoeng";
+                  }
+                  return null;
+                }
+                ),
+              TextFormField(
+                controller: controller.passwordController,
+                decoration: InputDecoration(hintText: "Maukkan Password"),
+                validator: (value) {
+                  if (value!.length <5) {
+                    return "password tidak boleh kosong";
+                  }
+                  return null;
+                }
+              ),
+              Obx(() => controller.loading.value?
+              CircularProgressIndicator():
+              ElevatedButton(onPressed: (){
+                controller.login();
+              }, child: Text("Login"),
+              )
+              )
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
